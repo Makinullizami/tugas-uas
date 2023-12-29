d4c5294de9f461ffda992efcf67b532518004db6
Makinullizami <agimbal65@gmail.com>
2023-12-29 13:52:46 +0700

    mengalami perubahan

--- lizam_store/settings.py
+++ lizam_store/settings.py
@@ -76,14 +76,7 @@ WSGI_APPLICATION = 'lizam_store.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'aplikasi_utama',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+    'default': {}
 }
 
 
